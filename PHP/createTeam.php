c3fb3fc218f6a75c2b4f7737aada29c2cc97a827
lyman l shen <lshen@linux11102.dc.engr.scu.edu>
2019-11-03 14:04:33 -0800

    createteam works; will change when login system is added

--- PHP/createTeam.php
+++ PHP/createTeam.php
@@ -1,6 +1,8 @@
 <?php
   if(!empty($_POST)) {
     $groupName = $_POST['groupName'];
+    //userEmail will be $_SESSION['userEmail'] after login system is done
+    $userEmail = $_POST['userEmail'];
     require '/webpages/lshen/dbInfo.php';
     $conn=oci_connect($uname,$pword,$db);
     if(!$conn) {
@@ -12,7 +14,7 @@
     $sql = "BEGIN createteam(:groupname,:useremail); END;";
     $query = oci_parse($conn,$sql);
     oci_bind_by_name($query, ':groupname',$groupName);
-    oci_bind_by_name($query, ':useremail',);
+    oci_bind_by_name($query, ':useremail',$userEmail);
     if(oci_execute($query)) {
       echo 'Group successfully created.';
     }
