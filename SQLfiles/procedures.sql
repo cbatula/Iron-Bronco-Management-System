bec65f85622366c10cea57a26869c3a5fbc4b328
lyman l shen <lshen@linux11107.dc.engr.scu.edu>
2019-11-04 11:15:44 -0800

    Prepare to use function instead for joinTeam so know if user actually joined

--- SQLfiles/procedures.sql
+++ SQLfiles/procedures.sql
@@ -33,3 +33,22 @@ BEGIN
   END IF;
 END;
 /
+
+CREATE OR REPLACE FUNCTION joinTeamStatus
+(
+  groupName0 IN team.groupname%type,
+  userEmail0 IN members.useremail%type
+) RETURN number
+IS
+  numMembers numer;
+  gID number;
+BEGIN
+  SELECT groupId INTO gId FROM team WHERE groupName = groupName0;
+  SELECT COUNT(*) INTO numMembers FROM members WHERE groupId = gId;
+  IF (numMembers < 3)
+  THEN
+    INSERT INTO Members VALUES (gId,userEmail0);
+  END IF;
+  RETURN numMembers;
+END;
+/
