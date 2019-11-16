dde818c760abf108417025b1489d5b66ea183b50
Lyman Shen <lyman.shen@gmail.com>
2019-11-16 14:25:04 -0800

    Set user name as session variable

--- PHP/login.php
+++ PHP/login.php
@@ -1,37 +1,28 @@
 <?php
 $c = OCILogon("lshen", "password",'//dbserver.engr.scu.edu/db11g');
 
-if($c)
-{
+if($c) {
 	echo "Connected to Oracle\n";
-	
 	session_name( 'user' );
-        session_start();
+  session_start();
 
 	$email=$_POST["email"];
 	$password=$_POST["password"];	
 
-	$stid_get = oci_parse($c, "SELECT curpassword FROM participant WHERE email = :userEmail");
+	$stid_get = oci_parse($c, "SELECT curpassword, name FROM participant WHERE email = :userEmail");
 
  	oci_bind_by_name($stid_get,':userEmail',$email);
-	
-
-        $tf = oci_execute($stid_get);
-
-        if(!$tf)
-	{
+  $tf = oci_execute($stid_get);
+  if(!$tf){
 		echo "Error in get statement";
-                exit;
-        }
-               	
+    exit;
+  }             	
 	$row = oci_fetch_assoc($stid_get);
 
-//	if(!$row){}
-	
-	if(strcmp($row['CURPASSWORD'],$_POST['password']) == 0)
-	{
+	if(strcmp($row['CURPASSWORD'],$_POST['password']) == 0) {
 		$_SESSION["email"] = $email;
-		
+    $_SESSION['name'] = $row['NAME'];
+
 		$stid = oci_parse($c, "SELECT groupId FROM Members WHERE email = :UserEmail");
 		oci_bind_by_name($stid, ':UserEmail', $email);
 		oci_execute($stid);
@@ -48,9 +39,7 @@ if($c)
 		OCILogoff($c);
 		header("Location: home.php");
 		exit();
-	}
-	else
-	{	
+	} else {	
 		echo "Incorrect Password try again";
 		oci_commit($c);
 		OCILogoff($c);
