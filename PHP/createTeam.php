c30398ca1b407d5a9fbcfb6080174e43e050fd12
Lyman Shen <lyman.shen@gmail.com>
2019-11-17 17:23:52 -0800

    Better styling for create team page

--- PHP/createTeam.php
+++ PHP/createTeam.php
@@ -2,22 +2,28 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Create Team Test</title>
+  <title>Create Team Request</title>
+  <link rel="stylesheet" type="text/css" href="../CSS/template.css" />
 </head>
 <body>
+<img class="logo" src="https://www.scu.edu/media/offices/umc/scu-brand-guidelines/visual-identity-amp-photography/visual-identity-toolkit/logos-amp-seals/Mission-Horizontal-PMS201.png" alt="Trulli">
 <?php
 session_name( 'user' );
 session_start();
+$_SESSION['email'] = 'a7@scu.edu';
 $conn = OCILogon("lshen", "password",'//dbserver.engr.scu.edu/db11g');
 if(!empty($_SESSION) && isset($_SESSION['email'])) {
-  print($_SESSION['email']);
   if(empty($_POST)) {
-    echo '<h1> Create Team </h1>';
+    echo '<div class="header"> <h1> Create Team</h1> </div>';
+    echo '<div class="center">';
     echo '<form action="./createTeam.php" method="post">';
-    echo '<label for="groupName">Group Name: </label> <br />';
-    echo '<input type="text" name="groupName" id="groupName" /> <br />';
+    echo '<fieldset class="fieldset-auto-width">';
+    echo '<label for="groupName">Group Name: </label> <br /> <br />';
+    echo '<input type="text" name="groupName" id="groupName" /> <br /> <br />';
     echo '<input type="submit" value="Submit"/>';
+    echo '</fieldset>';
     echo '</form>';
+    echo '</div>';
   } else {
     $groupName = $_POST['groupName'];
     $userEmail = $_SESSION['email'];
