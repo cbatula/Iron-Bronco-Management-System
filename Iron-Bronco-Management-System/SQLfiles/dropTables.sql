d7baa3853a0a95db00d4c779cbf510ab98fc9a94
Lyman Shen <lyman.shen@gmail.com>
2019-12-03 16:54:58 -0800

    Description

--- Iron-Bronco-Management-System/SQLfiles/dropTables.sql
+++ Iron-Bronco-Management-System/SQLfiles/dropTables.sql
@@ -1,3 +1,4 @@
+/* This script can be used to drop all tables. Use in conjunction with createTables.sql for full reset on database*/
 DROP TABLE Group_Requests;
 DROP TABLE Team_Requests;
 DROP TABLE Race_Progress;
