03c3c25140e51845b55f7d1cd9f1cd5deb39397d
Angela Shao <ashao@scu.edu>
2019-12-03 16:59:43 -0800

    Update sanitize.php

--- Iron-Bronco-Management-System/PHP/sanitize.php
+++ Iron-Bronco-Management-System/PHP/sanitize.php
@@ -1,5 +1,6 @@
 <?php
 
+// function to sanitize input; file is included and used in all relevant files
 function prepareInput($inputData){
 	$inputData = trim($inputData);
 	$inputData = htmlspecialchars($inputData);
